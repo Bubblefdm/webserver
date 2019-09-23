5275539a00467525e66cab773631cd79f4616bc4
Bubblefdm@163.com <Tianzi1314>
2019-09-23 23:15:16 +0800

    打包war

--- src/main/java/com/stopec/gy/GyApplication.java
+++ src/main/java/com/stopec/gy/GyApplication.java
@@ -2,11 +2,16 @@ package com.stopec.gy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GyApplication {
-
+public class GyApplication  extends SpringBootServletInitializer {
+//    //    使用自带tomcat启动
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(GyApplication.class);
+//    }
     public static void main(String[] args) {
         SpringApplication.run(GyApplication.class, args);
     }
